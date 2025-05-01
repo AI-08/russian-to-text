44e579586208c91310d010dc97de3e64a9ac7e48
AI-08 <marymorgan04@mail.ru>
2025-05-01 15:35:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ import streamlit as st
 from transformers import pipeline
 from PIL import Image
 
-code[class*="language-"], pre[class*="language-"]
-  text-shadow: 0 1px #14161800 !important
-  background: #242424 !important
-  span.token.operator
-    background: none
-  span.token.keyword
-    color: #866cba
-
 def load_image():
     uploaded_file = st.file_uploader(label='OCRусский - распознай русский текст с изображения')
     if uploaded_file is not None:
